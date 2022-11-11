596f20424da67a551f793b85ffa9c1658e46ddc3
Chang hun <nick6253@naver.com>
2022-11-11 23:38:22 +0900

    Level 1 / 2072. 홀수만 더하기

---
+++ Level 1/SWEA_2072.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    T = int(input())
+
+    for test_case in range(1, T + 1):
+        num_list = list(map(int, input().split()))
+        odd_nums = [x for x in num_list if x % 2 == 1]
+        print("#{} {}".format(test_case, sum(odd_nums)))
