771ff214619b93cef285c9e6fc6912e0f5de8f46
Chang hun <nick6253@naver.com>
2022-11-12 03:27:18 +0900

    Level 1 / 2029. 몫과 나머지 출력하기

--- Level 1/SWEA_2029.py
+++ Level 1/SWEA_2029.py
@@ -2,9 +2,6 @@ if __name__ == '__main__':
     T = int(input())
 
     for test_case in range(1, T + 1):
-        num_list = list(map(int, input().split()))
-
-        a = num_list[0]
-        b = num_list[1]
+        a, b = map(int, input().split())
 
         print(f"#{test_case} {a // b} {a % b}")
