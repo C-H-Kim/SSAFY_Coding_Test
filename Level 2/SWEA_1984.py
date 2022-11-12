d1fb9b5e01f733a5a23b0a2162bb2ec5e4428d43
Chang hun <nick6253@naver.com>
2022-11-13 08:01:32 +0900

    Level 2 / 1984. 중간 평균값 구하기

--- Level 2/SWEA_1984.py
+++ Level 2/SWEA_1984.py
@@ -2,3 +2,12 @@ if __name__ == '__main__':
     T = int(input())
 
     for test_case in range(1, T + 1):
+        num_list = list(map(int, input().split()))
+
+        num_list.remove(max(num_list))
+        num_list.remove(min(num_list))
+
+        avg = sum(num_list) / len(num_list)
+        avg = round(avg)
+
+        print(f"#{test_case} {avg}")
