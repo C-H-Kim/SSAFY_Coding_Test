6d993c9431b7f7ca9c345a0571a23b5df532ddf4
C-H-Kim <nick6253@naver.com>
2022-12-02 18:22:55 +0900

    Level 2 / 1970. 쉬운 거스름돈

--- Level 2/SWEA_1970.py
+++ Level 2/SWEA_1970.py
@@ -13,11 +13,14 @@ for test_case in range(1, T + 1):
 
         output_str = output_str + str(cnt) + " "
 
+        #나누는 수의 맨 앞자리가 5이면 5로 나눈다. -> 다음 나누는 수는 이전 나누는 수의 1/5이기 때문
         if divisor_cnt % 2 == 0:
             divisor = divisor // 5
+        #나누는 수의 맨 앞자리가 1이면 2로 나눈다. -> 다음 나누는 수는 이전 나누는 수의 절반이기 때문
         else:
             divisor = divisor // 2
 
+        #나누는 수의 순서를 파악하기 위한 변수 +1
         divisor_cnt += 1
 
     print(f"#{test_case}")
